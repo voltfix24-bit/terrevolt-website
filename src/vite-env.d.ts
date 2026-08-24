6b5837582dab69e19b125d7b46b196114e211167
voltfix24-bit <voltfix24@gmail.com>
2026-08-24 22:05:04 +0200

    Add asset JSON module typing

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare module "*.asset.json" {
+  const asset: {
+    url: string;
+    original_filename?: string;
+    content_type?: string;
+    size?: number;
+  };
+  export default asset;
+}
